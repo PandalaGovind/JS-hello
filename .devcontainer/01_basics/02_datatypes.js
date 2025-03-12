57642cde7def1d77b1ed0fba62eb82ac75893eb6
PandalaGovind <163167235+PandalaGovind@users.noreply.github.com>
2025-03-12 12:06:35 +0000

    variables and data types

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3) //code readability should be high
+console.log("Shri Ganeshay Namah")
+let name="Pandala Govind"
+let age=20
+
+//number=2 to the power 53
+// bigint
+// string-->" "
+// boolean--> true/false
+// null--> standalone value
+// undefined-->
+// symbol--> unique
+
+// object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
