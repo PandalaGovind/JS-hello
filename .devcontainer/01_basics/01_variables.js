924b5e72fcf705597f6cbbcd72202c8f980860d2
PandalaGovind <163167235+PandalaGovind@users.noreply.github.com>
2025-03-12 12:07:33 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const registration_no=20233207
+const Name="Pandala Govind"
+let city="Kanpur"
+var state="Uttar Pradesh"
+let locality
+
+console.table([registration_no, Name, city, state, locality])
