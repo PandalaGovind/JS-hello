ea458aac628bbd8da727c0ad251fe71a867bc57e
PandalaGovind <163167235+PandalaGovind@users.noreply.github.com>
2025-03-19 02:27:40 +0530

    Update MyProject.js

--- color-changer/MyProject.js
+++ color-changer/MyProject.js
@@ -1,22 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let buttons = document.querySelectorAll('.button');
-  buttons.forEach(function (button) {
+console.log("hitesh")
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
   console.log(button);
-  button.addEventlistener('click', function (e) {
+  button.addEventListener('click', function (e) {
     console.log(e);
-    console.log(e.target.id);
-    if(e.target.id=='grey'){
-      document.body.style.backgroundColor=e.target.id;
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
     }
-    if(e.target.id=='white'){
-      document.body.style.backgroundColor=e.target.id;
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
     }
-    if(e.target.id=='blue'){
-      document.body.style.backgroundColor=e.target.id;
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
     }
-    if(e.target.id=='yellow'){
-      document.body.style.backgroundColor=e.target.id;
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
     }
+    
   });
 });
-});
+
