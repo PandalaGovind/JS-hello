6475f52ac461ceb7fbce90dc4b270ffdd145d75c
PandalaGovind <163167235+PandalaGovind@users.noreply.github.com>
2025-03-19 02:14:11 +0530

    Create MyProject.js

---
+++ color-changer/MyProject.js
@@ -0,0 +1,19 @@
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventlistener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if(e.target=='grey'){
+      body.style.backgroundColor=e.target.id;
+    }
+    if(e.target=='white'){
+      body.style.backgroundColor=e.target.id;
+    }
+    if(e.target=='blue'){
+      body.style.backgroundColor=e.target.id;
+    }
+    if(e.target=='yellow'){
+      body.style.backgroundColor=e.target.id;
+    }
+  });
+});
